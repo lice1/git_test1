d5225661a27f62f30ddb4180b9102de3cb4983de
lice <1293308187@qq.com>
2022-07-11 14:48:31 +0800

    third commit

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -2,6 +2,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git2");
+        System.out.println("hello,git3");
 
     }
 }
